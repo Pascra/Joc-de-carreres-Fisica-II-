df388ad3914affed7c441c4ff8c90e89051d7cd6
Pascra <arnaupascual40@gmail.com>
2024-12-11 14:19:56 +0100

    Añadir sprites,mapa,scene.cpp y .h editar main.cpp y otros codigos
    Co-Authored-By: Roster247 <160216328+Roster247@users.noreply.github.com>

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,87 +1,36 @@
-#include "Application.h"
-#include "Globals.h"
-
+// main.cpp
+#include "Scene.h"
 #include "raylib.h"
+#include <iostream>
 
-#include <stdlib.h>
+#define TITLE "Raylib TMX Viewer"
 
-enum main_states
-{
-	MAIN_CREATION,
-	MAIN_START,
-	MAIN_UPDATE,
-	MAIN_FINISH,
-	MAIN_EXIT
-};
+int main() {
+    // Inicializa la ventana
+    InitWindow(800, 600, TITLE);
+    SetTargetFPS(60);
 
-int main(int argc, char ** argv)
-{
-	LOG("Starting game '%s'...", TITLE);
+    Scene scene;
 
-	int main_return = EXIT_FAILURE;
-	main_states state = MAIN_CREATION;
-	Application* App = NULL;
+    if (!scene.LoadMap("Assets/CircuitoFinalTotal.tmx")) {
+        std::cerr << "Failed to load map!" << std::endl;
+        CloseWindow();
+        return -1;
+    }
 
-	while (state != MAIN_EXIT)
-	{
-		switch (state)
-		{
-		case MAIN_CREATION:
+    while (!WindowShouldClose()) {
+        scene.Update(); // Actualiza la lógica de la escena
 
-			LOG("-------------- Application Creation --------------");
-			App = new Application();
-			state = MAIN_START;
-			break;
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
 
-		case MAIN_START:
+        scene.Render(); // Renderiza el mapa
 
-			LOG("-------------- Application Init --------------");
-			if (App->Init() == false)
-			{
-				LOG("Application Init exits with ERROR");
-				state = MAIN_EXIT;
-			}
-			else
-			{
-				state = MAIN_UPDATE;
-				LOG("-------------- Application Update --------------");
-			}
+        EndDrawing();
+    }
 
-			break;
+    scene.Unload(); // Limpia recursos
+    CloseWindow();
 
-		case MAIN_UPDATE:
-		{
-			int update_return = App->Update();
-
-			if (update_return == UPDATE_ERROR)
-			{
-				LOG("Application Update exits with ERROR");
-				state = MAIN_EXIT;
-			}
-
-			if (update_return == UPDATE_STOP)
-				state = MAIN_FINISH;
-		}
-			break;
-
-		case MAIN_FINISH:
-
-			LOG("-------------- Application CleanUp --------------");
-			if (App->CleanUp() == false)
-			{
-				LOG("Application CleanUp exits with ERROR");
-			}
-			else
-				main_return = EXIT_SUCCESS;
-
-			state = MAIN_EXIT;
-
-			break;
-
-		}
-	}
-
-	delete App;
-	LOG("Exiting game '%s'...\n", TITLE);
-	return main_return;
+    return 0;
 }
