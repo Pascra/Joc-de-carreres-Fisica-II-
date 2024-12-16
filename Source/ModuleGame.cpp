33ffd205e7fc0902cd383bc5871e7c95d3379d0b
Pascra <arnaupascual40@gmail.com>
2024-12-16 10:30:27 +0100

    añadir car

--- Source/ModuleGame.cpp
+++ Source/ModuleGame.cpp
@@ -166,6 +166,7 @@ bool ModuleGame::Start()
 	circle = LoadTexture("Assets/wheel.png"); 
 	box = LoadTexture("Assets/crate.png");
 	rick = LoadTexture("Assets/rick_head.png");
+	car = LoadTexture("Assets/pitstop_car_3_right.png");
 	
 	bonus_fx = App->audio->LoadFx("Assets/bonus.wav");
 
