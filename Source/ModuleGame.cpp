2edd7bd14d25a4d3ced6f2391b6a358f35fd3bc0
Pascra <160216328+Roster247@users.noreply.github.com>
2025-01-04 18:41:45 +0100

    Colisiones finales e AI retocada

--- Source/ModuleGame.cpp
+++ Source/ModuleGame.cpp
@@ -32,36 +32,331 @@ protected:
     Module* listener = nullptr;
 };
 
-class Rick : public PhysicEntity
+class Borde1 : public PhysicEntity
 {
 public:
     // Pivot 0, 0
-    static constexpr int rick_head[40] = {
-        104, 140,
-        105, 118,
-        108, 103,
-        114, 88,
-        126, 66,
-        138, 53,
-        155, 37,
-        178, 23,
-        200, 16,
-        218, 13,
-        233, 12,
-        233, 0,
-        192, 0,
-        183, 1,
-        154, 14,
-        132, 30,
-        111, 54,
-        94, 88,
-        88, 119,
-        88, 140,
+    static constexpr int borde1[348] = {
+        301, 1021,
+        804, 1022,
+        809, 1020,
+        820, 1017,
+        831, 1013,
+        845, 1009,
+        852, 1004,
+        873, 996,
+        885, 988,
+        899, 974,
+908, 960,
+924, 937,
+940, 912,
+945, 896,
+950, 887,
+957, 860,
+959, 833,
+958, 820,
+958, 183,
+957, 173,
+956, 144,
+949, 125,
+941, 103,
+926, 79,
+909, 61,
+891, 41,
+874, 27,
+856, 17,
+838, 8,
+818, 2,
+323, 2,
+184, 2,
+166, 8,
+154, 16,
+132, 30,
+118, 48,
+106, 65,
+100, 79,
+94, 96,
+91, 107,
+89, 121,
+89, 140,
+89, 159,
+91, 171,
+93, 184,
+99, 197,
+105, 211,
+116, 229,
+126, 240,
+145, 260,
+176, 287,
+192, 303,
+208, 323,
+247, 362,
+283, 395,
+293, 396,
+305, 398,
+315, 403,
+332, 411,
+344, 418,
+360, 425,
+379, 444,
+399, 443,
+414, 442,
+434, 440,
+444, 436,
+458, 430,
+474, 421,
+493, 406,
+505, 393,
+513, 381,
+521, 366,
+528, 348,
+533, 326,
+534, 321,
+534, 300,
+535, 284,
+535, 266,
+539, 258,
+547, 252,
+552, 250,
+603, 250,
+618, 250,
+625, 253,
+632, 262,
+631, 271,
+628, 281,
+621, 287,
+610, 301,
+601, 318,
+595, 339,
+590, 361,
+586, 368,
+584, 378,
+578, 387,
+577, 396,
+568, 401,
+568, 409,
+558, 420,
+506, 472,
+481, 496,
+465, 513,
+434, 545,
+424, 560,
+420, 566,
+417, 552,
+409, 549,
+408, 534,
+407, 527,
+405, 512,
+401, 495,
+397, 482,
+390, 470,
+384, 459,
+375, 447,
+366, 437,
+357, 429,
+346, 424,
+325, 413,
+308, 404,
+296, 401,
+292, 400,
+264, 400,
+257, 395,
+170, 395,
+164, 399,
+141, 400,
+124, 400,
+98, 408,
+81, 415,
+62, 426,
+56, 433,
+39, 450,
+26, 467,
+17, 489,
+11, 512,
+9, 523,
+9, 541,
+10, 565,
+11, 570,
+16, 593,
+26, 614,
+35, 629,
+46, 642,
+59, 655,
+72, 663,
+90, 673,
+108, 680,
+125, 684,
+138, 685,
+154, 685,
+256, 684,
+264, 688,
+273, 689,
+272, 697,
+277, 707,
+258, 726,
+238, 743,
+209, 767,
+179, 796,
+163, 821,
+156, 846,
+154, 874,
+154, 897,
+157, 915,
+165, 939,
+180, 965,
+192, 980,
+211, 997,
+230, 1008,
+248, 1014,
+273, 1019,
+287, 1021,
+298, 1021
 
     };
 
-    Rick(ModulePhysics* physics, int _x, int _y, Module* listener, Texture2D _texture)
-        : PhysicEntity(physics->CreateChain(0,0, rick_head, 40), listener)
+    Borde1(ModulePhysics* physics, int _x, int _y, Module* listener, Texture2D _texture)
+        : PhysicEntity(physics->CreateChain(0,0, borde1, 348), listener)
+        , texture(_texture)
+    {
+        ctype = CollisionType::DEFAULT;
+    }
+
+    void Update() override
+    {
+        int x, y;
+        body->GetPhysicPosition(x, y);
+        DrawTextureEx(texture, Vector2{ (float)x, (float)y }, body->GetRotation() * RAD2DEG, 1.0f, WHITE);
+    }
+
+private:
+    Texture2D texture;
+};
+
+class Borde2 : public PhysicEntity
+{
+public:
+    // Pivot 0, 0
+    static constexpr int borde2[228] = {
+        281, 878,
+    283, 870,
+    298, 856,
+    322, 837,
+    335, 826,
+    358, 800,
+    382, 772,
+    393, 755,
+    401, 735,
+    406, 727,
+    406, 701,
+    401, 692,
+    402, 675,
+    414, 674,
+    425, 678,
+    454, 678,
+    469, 673,
+    497, 654,
+    514, 642,
+    524, 632,
+    566, 591,
+    605, 553,
+    631, 526,
+    660, 493,
+    681, 467,
+    690, 456,
+    700, 433,
+    707, 403,
+    714, 398,
+    713, 387,
+    721, 379,
+    726, 361,
+    731, 350,
+    740, 339,
+    748, 325,
+    758, 297,
+    760, 267,
+    759, 247,
+    755, 224,
+    747, 203,
+    733, 179,
+    719, 161,
+    699, 146,
+    676, 134,
+    650, 127,
+    628, 124,
+    608, 124,
+    556, 124,
+    544, 124,
+    527, 126,
+    513, 127,
+    496, 133,
+    479, 141,
+    462, 151,
+    452, 160,
+    442, 170,
+    433, 182,
+    424, 196,
+    418, 209,
+    413, 221,
+    411, 239,
+    408, 254,
+    408, 268,
+    401, 273,
+    397, 278,
+    397, 288,
+    407, 297,
+    408, 303,
+    403, 311,
+    396, 317,
+    389, 318,
+    382, 315,
+    368, 300,
+    350, 281,
+    244, 176,
+    224, 156,
+    216, 141,
+    217, 136,
+    222, 129,
+    230, 124,
+    240, 123,
+    251, 122,
+    456, 122,
+    555, 121,
+    642, 121,
+    716, 122,
+    765, 123,
+    787, 127,
+    805, 140,
+    815, 150,
+    823, 163,
+    827, 180,
+    828, 353,
+    829, 390,
+    829, 428,
+    830, 469,
+    831, 537,
+    832, 649,
+    832, 807,
+    832, 837,
+    829, 852,
+    818, 869,
+    814, 877,
+    804, 885,
+    795, 891,
+    782, 894,
+    775, 895,
+    752, 895,
+    674, 896,
+    512, 896,
+    411, 896,
+    300, 896,
+    289, 889,
+    283, 883
+
+
+    };
+
+    Borde2(ModulePhysics* physics, int _x, int _y, Module* listener, Texture2D _texture)
+        : PhysicEntity(physics->CreateChain(0, 0, borde2, 228), listener)
         , texture(_texture)
     {
         ctype = CollisionType::DEFAULT;
@@ -107,8 +402,9 @@ bool ModuleGame::Start()
 {
     TraceLog(LOG_INFO, "Loading game assets");
 
-    entities.emplace_back(new Rick(App->physics, 0, 0, this, default));
-   
+    entities.emplace_back(new Borde1(App->physics, 0, 0, this, default));
+    entities.emplace_back(new Borde2(App->physics, 0, 0, this, default));
+
     player1_win_texture = LoadTexture("Assets/player1_gana.png");
     if (player1_win_texture.id == 0)
     {
@@ -176,21 +472,38 @@ bool ModuleGame::Start()
 
     // Configuración inicial de la IA
     ai_position = { 913.7f, 525.0f }; // Cambia estas coordenadas según lo que desees
-    ai_rotation = 180.0f;             // Rotación inicial hacia la derecha
+    ai_rotation = 270.0f;             // Rotación inicial hacia la derecha
     ai_speed = 0.0f;
 
     // Crear cuerpo físico de la IA
     float hitbox_width = 50.0f;  // Ajusta según el tamaño del coche
     float hitbox_height = 30.0f; // Ajusta según el tamaño del coche
+
+    b2BodyDef body_def;
+    body_def.type = b2_dynamicBody;
+    body_def.position.Set(PIXEL_TO_METERS(ai_position.x), PIXEL_TO_METERS(ai_position.y));
+    body_def.angle = ai_rotation * DEG2RAD; // Rotación inicial en radianes
+
+    b2PolygonShape hitbox;
+    hitbox.SetAsBox(PIXEL_TO_METERS(hitbox_width * 0.5f), PIXEL_TO_METERS(hitbox_height * 0.5f));
+
+    b2FixtureDef fixture_def;
+    fixture_def.shape = &hitbox;
+    fixture_def.density = 1.0f;
+    fixture_def.friction = 0.3f;
+    fixture_def.restitution = 0.0f;
+
     ai_body = App->physics->CreateRectangle(ai_position.x, ai_position.y, hitbox_width, hitbox_height);
-    ai_body->body->SetType(b2_dynamicBody); // Configurar como cuerpo dinámico
+    ai_body->body->SetGravityScale(0); // Configurar gravedad para que no afecte a la IA
     ai_body->ctype = CollisionType::AI;
-    // Ajustar la rotación inicial
-    ai_body->body->SetTransform(
-        b2Vec2(PIXEL_TO_METERS(ai_position.x), PIXEL_TO_METERS(ai_position.y)),
-        ai_rotation * DEG2RAD);
-    //Empieza a contar el tiempo
+
+    // Establecer velocidades iniciales a 0
+    ai_body->body->SetLinearVelocity(b2Vec2(0.0f, 0.0f));
+    ai_body->body->SetAngularVelocity(0.0f);
+
+    // Empieza a contar el tiempo
     time = 0.0f;
+
     return true;
 }
 
@@ -270,7 +583,7 @@ update_status ModuleGame::Update()
                 // Reiniciar tras completar una vuelta
                 current_checkpoint_ai = 0;
                 ai_speed = 0.0f; // Reiniciar velocidad
-                ai_rotation = 180.0f; // Reiniciar rotación
+                ai_rotation = 270.0f; // Reiniciar rotación
                 ai_position = { 913.7f, 525.0f }; // Reiniciar posición inicial
                 ai_body->body->SetTransform(
                     b2Vec2(PIXEL_TO_METERS(ai_position.x), PIXEL_TO_METERS(ai_position.y)),
@@ -289,7 +602,7 @@ update_status ModuleGame::Update()
             direction_x /= distance; // Normalizar el vector dirección
             direction_y /= distance;
 
-            ai_speed = 250.0f; // Velocidad constante de la IA
+            ai_speed = 225.0f; // Velocidad constante de la IA
 
             // Calcular la rotación hacia el checkpoint
             float target_rotation = atan2(direction_y, direction_x) * RAD2DEG;
@@ -302,7 +615,7 @@ update_status ModuleGame::Update()
             }
 
             // Ajustar rotación progresivamente
-            float rotation_speed = 130.0f * GetFrameTime();
+            float rotation_speed = 150.0f * GetFrameTime();
             if (fabs(delta_rotation) > rotation_speed)
             {
                 ai_rotation += (delta_rotation > 0) ? rotation_speed : -rotation_speed;
